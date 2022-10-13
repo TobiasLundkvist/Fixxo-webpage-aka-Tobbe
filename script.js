fdeebeee59e6dfe38507275d5ebd646d8435fe56
Tobias Lundkvist <tobias_lundkvist@hotmail.com>
2022-10-13 12:34:32 +0200

    form site done and script started

---
+++ script.js
@@ -0,0 +1,33 @@
+// const submitForm = (e) => {
+//     e.preventDefault()
+//     validate(e)
+// }
+
+// const validate = (e) => {
+//     let target
+
+//     switch(e.type) {
+//         case "submit":
+//             break;
+        
+//         case "keyup":
+//             target = e.target
+//             break;
+//     }
+// }
+
+const = checklength = (value, minLength = 2) => {
+
+}
+
+
+const validate = (e) => {
+    switch(e.target.type) {
+        case "text":
+            break;
+        case "email":
+            break;
+        case "textarea":
+            break;
+    }
+}

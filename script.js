030b6d260c9d392844895e893c004147ff488419
Tobias Lundkvist <tobias_lundkvist@hotmail.com>
2022-10-13 16:46:49 +0200

    enklare javascript påbörjat, kommentarer och footer tillagt

--- script.js
+++ script.js
@@ -16,18 +16,45 @@
 //     }
 // }
 
-const = checklength = (value, minLength = 2) => {
+const checkLength = (element, minLength = 2, message) => {
+    
+    if(message === undefined)
+        message = `Your ${element.target.id} must contain at least ${minLength} characters`
 
+    if(element.target.value.length < minLength) {
+        document.getElementById(element.target.id).classList.add('error')
+        document.getElementById(`${element.target.id}ErrorMessage`).innerText = message
+    } else {
+        document.getElementById(element.target.id).classList.remove('error')
+        document.getElementById(`${element.target.id}ErrorMessage`).innerText = ''
+    }
 }
+
+const checkEmail = (element, message) => {
+    if(message === undefined)
+        message = `Your ${element.target.id} must be a valid E-mail`
+
+    if (!element.target.value.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
+        document.getElementById(element.target.id).classList.add('error')
+        document.getElementById(`${element.target.id}ErrorMessage`).innerText = message
+    } else {
+        document.getElementById(element.target.id).classList.remove('error')
+        document.getElementById(`${element.target.id}ErrorMessage`).innerText = ''
+    }
+}
+
 
 
 const validate = (e) => {
     switch(e.target.type) {
         case "text":
+            checkLength(e)
             break;
         case "email":
+            checkEmail(e)
             break;
         case "textarea":
+            checkLength(e, 5)
             break;
     }
 }
